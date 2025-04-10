0eee16d4407ea522841376cce3221de797f24a12
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-10 14:55:02 +0530

    Create 5_DutchNationalFlag.cpp
    Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
    
    We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
    
    You must solve this problem without using the library's sort function.
    
     
    
    Example 1:
    
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]
    Example 2:
    
    Input: nums = [2,0,1]
    Output: [0,1,2]

---
+++ Day 1 : Arrays/5_DutchNationalFlag.cpp
@@ -0,0 +1,23 @@
+// SORT Array Of 0's , 1's & 2's
+/*
+   Question Link --> https://leetcode.com/problems/sort-colors/description/
+*/
+//T.C. = O(N) , S.C. = O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int left=0,mid;
+        int right=arr.size()-1;
+
+        while(mid<=right){
+            if(arr[mid]==0){
+                swap(arr[mid++],arr[left++]);
+            }
+            else if(arr[mid]==1)mid++;
+            else{ //==2
+                swap(arr[mid],arr[right--]);
+            }
+        }
+    }
+};

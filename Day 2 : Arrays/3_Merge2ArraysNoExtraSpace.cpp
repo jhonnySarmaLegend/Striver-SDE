bd0694cff763c56786e8dcdd2cf90fa87d3eeb37
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-11 14:32:08 +0530

    Create 3_Merge2ArraysNoExtraSpace.cpp
    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
    Output: [1,2,2,3,5,6]
    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
    Example 2:
    
    Input: nums1 = [1], m = 1, nums2 = [], n = 0
    Output: [1]
    Explanation: The arrays we are merging are [1] and [].
    The result of the merge is [1].
    Example 3:
    
    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
    Output: [1]
    Explanation: The arrays we are merging are [] and [1].
    The result of the merge is [1].
    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.

---
+++ Day 2 : Arrays/3_Merge2ArraysNoExtraSpace.cpp
@@ -0,0 +1,59 @@
+/*
+   Question Link --> https://leetcode.com/problems/merge-sorted-array/description/
+*/
+
+// Method 1
+//T.C. = O(N+M) , S.C. = O(1)
+void merge(long long arr1[], long long arr2[], int n, int m) {
+
+    //Declare 2 pointers:
+    int left = n - 1;
+    int right = 0;
+
+    //Swap the elements until arr1[left] is
+    // smaller than arr2[right]:
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    // Sort arr1[] and arr2[] individually:
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
+}
+
+// METHOD 2 - GAP METHOD
+// Variation 2 - length of 2 arrays different
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+       int i = m - 1;       // Index for nums1
+    int j = n - 1;       // Index for nums2
+    int k = m + n - 1;   // Index for merged array (nums1)
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // Copy any remaining elements from nums2 to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+
+    }
+};

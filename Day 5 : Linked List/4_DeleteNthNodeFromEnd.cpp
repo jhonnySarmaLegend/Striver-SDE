d240b6559051a84568d3052ffe80819e2564347d
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-21 14:45:48 +0530

    Create 4_DeleteNthNodeFromEnd.cpp

---
+++ Day 5 : Linked List/4_DeleteNthNodeFromEnd.cpp
@@ -0,0 +1,68 @@
+/*
+   Question Link --> https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+*/
+// OPTIMAL --> T.C. = O(N) , S.C. = O(1)
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        for(int i=0;i<n;i++)fast=fast->next;
+
+        if(fast==NULL)return head->next;
+
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+  
+        slow->next=slow->next->next;
+  
+        return head;
+    }
+
+
+
+
+
+// BRUTE FORCE 
+//  T.C. = O(L)+O(L-N) , S.C. = O(1)
+Node* DeleteNthNodefromEnd(Node* head, int N) {
+    if (head == NULL) {
+        return NULL;
+    }
+    int cnt = 0;
+    Node* temp = head;
+
+    // Count the number of nodes in the linked list
+    while (temp != NULL) {
+        cnt++;
+        temp = temp->next;
+    }
+
+    // If N equals the total number of
+    // nodes, delete the head
+    if (cnt == N) {
+        Node* newhead = head->next;
+        delete (head);
+        return newhead;
+    }
+
+    // Calculate the position of the node to delete (res)
+    int res = cnt - N;
+    temp = head;
+
+    // Traverse to the node just before the one to delete
+    while (temp != NULL) {
+        res--;
+        if (res == 0) {
+            break;
+        }
+        temp = temp->next;
+    }
+
+    // Delete the Nth node from the end
+    Node* delNode = temp->next;
+    temp->next = temp->next->next;
+    delete (delNode);
+    return head;
+}

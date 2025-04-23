9e83f39f34328d0e5ddb10312f5bd0d7a86d3f0c
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-23 10:20:38 +0530

    Create 4_LLPalindromeOrNot.cpp

---
+++ Day 6 : Linked Lists/4_LLPalindromeOrNot.cpp
@@ -0,0 +1,69 @@
+/*
+   Question Link --> https://leetcode.com/problems/palindrome-linked-list/description/
+*/
+
+//BRUTE FORCE
+bool isPalindrome(ListNode* head) {
+        vector<int> num;
+        while(head){
+            num.push_back(head->val);
+            head=head->next;
+        }
+        vector<int> numRev(num.rbegin(),num.rend());
+        return num==numRev;  
+    }
+
+//OPTIMAL
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  
+    ListNode* rev(ListNode* head){
+        ListNode* temp=head;
+        ListNode* prev=NULL;
+        while(temp!=NULL){
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+         return prev;
+    }
+   
+
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+       ListNode* prev= rev(slow->next);
+       slow->next=prev;
+
+       ListNode* temp1=head;
+       ListNode* temp2=prev;
+
+       while(temp2!=NULL){
+        if(temp1->val!=temp2->val)return false;
+
+        temp1=temp1->next;
+         temp2=temp2->next;
+
+       }
+
+       return true;
+
+    }
+};

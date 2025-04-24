6f1450c67cfe061f43950f9723811010943d5af9
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-04-24 10:50:44 +0530

    Create 3_PostOrder.cpp

---
+++ Day 17 : Binary Tree/3_PostOrder.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+void PostOrder(TreeNode* root, vector<int> &ans){
+        if(root==NULL)return;
+
+         PostOrder(root->left,ans);
+         PostOrder(root->right,ans);
+         ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+         vector<int> ans;
+        PostOrder(root,ans);
+        return ans;
+    }
+};

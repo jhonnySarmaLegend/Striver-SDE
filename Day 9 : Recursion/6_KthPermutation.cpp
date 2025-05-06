359d6d65a8c5480a1ebbda7029bdbbcabcb97736
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-05-06 09:01:03 +0530

    Update 6_KthPermutation.cpp

--- Day 9 : Recursion/6_KthPermutation.cpp
+++ Day 9 : Recursion/6_KthPermutation.cpp
@@ -79,3 +79,33 @@ Reason: Result stored in a vector, we are auxiliary space taken by recursion
     return *it;
   }
 };
+
+
+//OPTIMAL 
+    string getPermutation(int n, int k) {
+      int fact = 1;
+      vector < int > numbers;
+      for (int i = 1; i < n; i++) {
+        fact = fact * i;
+        numbers.push_back(i);
+      }
+      numbers.push_back(n);
+      string ans = "";
+      k = k - 1;
+      while (true) {
+        ans = ans + to_string(numbers[k / fact]);
+        numbers.erase(numbers.begin() + k / fact);
+        if (numbers.size() == 0) {
+          break;
+        }
+
+        k = k % fact;
+        fact = fact / numbers.size();
+      }
+      return ans;
+    }
+
+/*
+Time Complexity: O(N) * O(N) = O(N^2)
+Space Complexity: O(N) 
+*/

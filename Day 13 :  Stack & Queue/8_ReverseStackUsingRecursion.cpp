4cce2320e3133363b0cb9e4f39d31fe75a6b644b
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-06-09 17:06:16 +0530

    Update 8_ReverseStackUsingRecursion.cpp

--- Day 13 :  Stack & Queue/8_ReverseStackUsingRecursion.cpp
+++ Day 13 :  Stack & Queue/8_ReverseStackUsingRecursion.cpp
@@ -1,5 +1,8 @@
 //Question Link-->https://www.geeksforgeeks.org/problems/reverse-a-stack/1
-
+/*
+Time Complexity: O(n*n) Since we need to call fun() and it recursively call insert_at_bottom which is again calling itself for recursion, hence O(n*n)
+Space Complexity: O(1) We need no extra stack for answer , hence O(1), but for recursion calls it needs o(n) auxiliary space.
+*/
 void insertBottom(stack<int> &st,int ele){
         //base case
         if(st.empty()){
@@ -27,3 +30,18 @@ void insertBottom(stack<int> &st,int ele){
         
         insertBottom(st,num);
     }
+
+
+//OPtimal 
+//T.C.==S.C.=O(N)
+  void Reverse(stack<int> &st) {
+        queue<int> q;
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        while(!q.empty()){
+            st.push(q.front());
+            q.pop();
+        }
+  }

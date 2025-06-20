c693c7a49127e74badf487007af054c08870146e
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-06-20 09:23:58 +0530

    Update 3_Largest_Rectangle_histogram.cpp

--- Day 14 : Stack and Queue/3_Largest_Rectangle_histogram.cpp
+++ Day 14 : Stack and Queue/3_Largest_Rectangle_histogram.cpp
@@ -33,3 +33,26 @@ public:
         return maxi;
     }
 };
+
+
+
+//OPTIMAL APPROACH 2 --> STRIVER
+int largestRectangleArea(vector < int > & histo) { // Function definition: Takes a reference to a vector of integers (histo), which represents the heights of the histogram bars. It returns an integer, which will be the maximum area found.
+      stack < int > st; // Declares a stack of integers. This stack will store indices of histogram bars.
+      int maxA = 0; // Initializes an integer variable 'maxA' to 0. This variable will store the maximum area found so far.
+      int n = histo.size(); // Stores the number of bars in the histogram in integer 'n'.
+      for (int i = 0; i <= n; i++) { // Loops through the histogram bars from index 0 up to and including 'n' (one past the last actual bar).
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) { // Inner loop: Continues as long as the stack is not empty AND (we are at the end of the histogram OR the current bar's height is less than or equal to the height of the bar at the top of the stack).
+          int height = histo[st.top()]; // Gets the height of the bar at the top of the stack (the bar being considered for an area calculation).
+          st.pop(); // Removes the top element from the stack.
+          int width; // Declares an integer variable 'width' to store the calculated width of the rectangle.
+          if (st.empty()) // If the stack becomes empty after popping the top element.
+            width = i; // The width extends from index 0 up to the current index 'i' (since there are no bars to its left that could limit it).
+          else // If the stack is not empty after popping.
+            width = i - st.top() - 1; // The width extends from the index of the new top of the stack + 1, up to the current index 'i' - 1. (i - (st.top() + 1)) = i - st.top() - 1
+          maxA = max(maxA, width * height); // Updates 'maxA' if the newly calculated area (width * height) is greater than the current 'maxA'.
+        } // End of while loop (processing popped bars).
+        st.push(i); // Pushes the current bar's index 'i' onto the stack.
+      } // End of for loop.
+      return maxA; // Returns the maximum area found after processing all bars.
+    }

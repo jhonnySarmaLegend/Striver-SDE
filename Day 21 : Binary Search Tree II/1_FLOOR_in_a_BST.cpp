6fce64bf1ae4a1e3ac728de62568c73886bcde36
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-06-25 15:14:54 +0530

    Create 1_FLOOR_in_a_BST.cpp

---
+++ Day 21 : Binary Search Tree II/1_FLOOR_in_a_BST.cpp
@@ -0,0 +1,38 @@
+//floor(x) <= x
+
+
+
+//ITERATIVE APPROACH
+int floorInBST(TreeNode *root,int key){
+ int floor=-1;
+  while(root){
+    if(root->val == key){
+     floor=root->val;
+     return floor;
+    }
+     else if(root->val < key){
+     floor=root->val;
+     root=root->right;
+    }
+    else{
+     root=root->left;
+    }
+  }
+ return floor;
+}
+
+
+//  RECURSIVE APPROACH
+// Function to find the largest value smaller than the key (Predecessor)
+    // Pass 'pre' by reference so it can be updated
+    void findSmall(Node* root, int key, Node*& pre) {
+        if (root == nullptr) return;
+
+        if (root->data < key) {
+            pre = root; // Update predecessor
+            findSmall(root->right, key, pre); // Look for a larger predecessor in the right subtree
+        } else {
+            // If current node's data >= key, move left to find smaller values
+            findSmall(root->left, key, pre);
+        }
+    }

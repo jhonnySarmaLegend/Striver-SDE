1eaab70c2497bc304e0f471afd389ca23ec86e7f
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-01 14:29:52 +0530

    Create 1_BFS.cpp

---
+++ Day 23 : Graphs/1_BFS.cpp
@@ -0,0 +1,28 @@
+//Question Link --> https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
+//Adjacency List
+
+vector<int> bfs(vector<vector<int>> &adj) {
+int v = adj.size(); 
+vector<int> vis(v, 0);
+queue<int> q; 
+
+q.push(0); 
+vis[0] = 1; 
+
+vector<int> ans; 
+
+while (!q.empty()) {
+    int node = q.front(); 
+    q.pop(); 
+    ans.push_back(node); 
+    for (auto it : adj[node]) { 
+        if (!vis[it]) { 
+            vis[it] = 1; 
+            q.push(it); 
+        }
+    }
+}
+return ans;
+}
+

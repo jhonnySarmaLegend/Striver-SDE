4ffa280cd26cfaa78926d13c4db5fbc128c85f9a
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-16 09:58:17 +0530

    Create 1_MinimumMeetingRooms.cpp

---
+++ Day 8 : Greedy Algorithm/1_MinimumMeetingRooms.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/dsa/meeting-rooms-find-minimum-meeting-rooms/#better-approach-using-sorting-and-two-pointers-onlogn-time-and-o1-space
+
+
+// Function to find the minimum number of rooms
+// required
+int minMeetingRooms(vector<int> &start, vector<int> &end) {
+    int n = start.size();
+
+    // no. of rooms at any point of time.
+    int room = 0;
+    int res = 0;
+
+    // sorting the start and end time of meetings
+    sort(start.begin(), start.end());
+    sort(end.begin(), end.end());
+
+    // pointing to the current index of start array and end array.
+    int i = 0, j = 0;
+
+    while (i < start.size()) {
+
+        // encountered start time of meeting.
+        if (start[i] < end[j]) {
+            // increase no of rooms
+            room++;
+            i++;
+        }
+
+        // encountered end time of meeting.
+        else {
+            // decrease no of rooms.
+            room--;
+            j++;
+        }
+        // updating final result
+        res = max(res, room);
+    }
+    return res;
+}

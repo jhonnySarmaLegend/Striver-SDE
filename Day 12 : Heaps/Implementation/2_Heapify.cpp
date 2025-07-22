571641a5a122f311f13cfc2565433e24fe6a07c2
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-22 12:32:29 +0530

    Create 2_Heapify.cpp

---
+++ Day 12 : Heaps/Implementation/2_Heapify.cpp
@@ -0,0 +1,40 @@
+// Array to heap array
+// 1 based indexing where arr[0]=-1
+// leaf node --> (n/2)+1  to n  index
+
+
+// MAX Heap --> 1 based indexing
+
+void heapify(int arr[],int n,int i){
+          int largest=i;
+          int leftIndex=2*i; 
+          int rightIndex=2*i + 1;
+          
+          if(leftIndex<n && arr[largest]<arr[leftIndex])largest=leftIndex; // for min heap  arr[smallest]>arr[leftIndex]
+          
+          if(rightIndex<n && arr[largest]<arr[rightIndex])largest=rightIndex; // for min heap  arr[smallest]>arr[rightIndex]
+          
+          if(largest!=i){
+              swap(arr[largest],arr[i]);
+              heapify(arr,n,largest);
+          }
+          
+          
+      }
+
+void main(){
+
+int arr[6]={-1,2,9,7,4,1};
+    int n=5;
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+
+}
+
+
+// 1-based indexing --> 2*i , 2*i + 1  for 0-based indexing 2*i + 1, 2*i + 2
+
+
+
+

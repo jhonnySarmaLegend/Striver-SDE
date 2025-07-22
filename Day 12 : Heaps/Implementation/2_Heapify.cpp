d68fc4cb3a56dcfea62c1b28b580cadffc3400f7
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-22 14:24:53 +0530

    Update 2_Heapify.cpp

--- Day 12 : Heaps/Implementation/2_Heapify.cpp
+++ Day 12 : Heaps/Implementation/2_Heapify.cpp
@@ -10,9 +10,9 @@ void heapify(int arr[],int n,int i){
           int leftIndex=2*i; 
           int rightIndex=2*i + 1;
           
-          if(leftIndex<n && arr[largest]<arr[leftIndex])largest=leftIndex; // for min heap  arr[smallest]>arr[leftIndex]
+          if(leftIndex<=n && arr[largest]<arr[leftIndex])largest=leftIndex; // for min heap  arr[smallest]>arr[leftIndex]
           
-          if(rightIndex<n && arr[largest]<arr[rightIndex])largest=rightIndex; // for min heap  arr[smallest]>arr[rightIndex]
+          if(rightIndex<=n && arr[largest]<arr[rightIndex])largest=rightIndex; // for min heap  arr[smallest]>arr[rightIndex]
           
           if(largest!=i){
               swap(arr[largest],arr[i]);
@@ -35,6 +35,6 @@ int arr[6]={-1,2,9,7,4,1};
 
 // 1-based indexing --> 2*i , 2*i + 1  for 0-based indexing 2*i + 1, 2*i + 2
 
-
+// min heap left<n
 
 

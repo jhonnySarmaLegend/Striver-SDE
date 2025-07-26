e2b78c1e61e3997d56d9012f0c933a53088986e0
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-26 14:45:15 +0530

    Update 1_Reverse Words in a Sentence.cpp

--- Day 15 : Strings I/1_Reverse Words in a Sentence.cpp
+++ Day 15 : Strings I/1_Reverse Words in a Sentence.cpp
@@ -1,7 +1,7 @@
 // Leetcode --> https://leetcode.com/problems/reverse-words-in-a-string/description/
 
 
-// Using stack
+// Using stack --> O(N) , O(N)
 class Solution {
 public:
     string reverseWords(string s) {
@@ -31,6 +31,73 @@ public:
      
       return res;
         
+    }
+};
+
+
+
+
+// OPTIMIZED  --> O(N) , O(1)
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0;
+        int right = s.length() - 1; // 'right' is used for the loop boundary check
+
+        string temp = ""; // Stores characters of the current word being built
+        string ans = "";  // Stores the final reversed sentence
+
+        // Iterate through the string from left to right
+        // The goal is to form individual words and then prepend them to 'ans'.
+        while (left <= right) {
+            char ch = s[left];
+
+            if (ch != ' ') {
+                // If the character is not a space, it's part of the current word
+                temp += ch; // Append character to the 'temp' word
+            } else { // if (ch == ' ')
+                // If a space is encountered, it signifies the end of a word (or multiple spaces)
+
+                // This condition is crucial for handling multiple spaces between words
+                // and leading spaces. If 'temp' is empty, it means we've just seen
+                // one or more consecutive spaces, so we don't form an empty word.
+                if (temp != "") {
+                    // If 'ans' already contains words, prepend the current 'temp' word
+                    // with a space separator.
+                    if (ans != "") {
+                        ans = temp + " " + ans;
+                    }
+                    // If 'ans' is empty, this is the first word being added.
+                    else {
+                        ans = temp;
+                    }
+                    temp = ""; // Reset 'temp' to start building the next word
+                }
+                // If temp is empty here, it means 'ch' was a space, but 'temp'
+                // was already empty (e.g., "  word", or "word  word").
+                // In this case, we simply do nothing and continue, effectively
+                // consuming the extra space.
+            }
+            left++; // Move to the next character
+        }
+
+        // After the loop, 'temp' might contain the last word of the string
+        // (if the string doesn't end with a space).
+        // This block handles appending that final word to 'ans'.
+        if (temp != "") {
+            // Same logic as inside the loop's 'else if (ch == ' ')' block:
+            // Prepend the last word to 'ans' with a space if 'ans' already has content.
+            if (ans != "") {
+                ans = temp + " " + ans;
+            }
+            // If 'ans' is still empty (e.g., input was "singleword"),
+            // then 'ans' simply becomes the last word.
+            else {
+                ans = temp;
+            }
+        }
+
+        return ans; // Return the string with words reversed
     }
 };
 

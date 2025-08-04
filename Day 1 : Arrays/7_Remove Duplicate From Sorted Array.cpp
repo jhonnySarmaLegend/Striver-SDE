3bcc2c7d9d60c10982b90ef556218f16169cf1b7
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-04 09:01:18 +0530

    Update 7_Remove Duplicate From Sorted Array.cpp

--- Day 1 : Arrays/7_Remove Duplicate From Sorted Array.cpp
+++ Day 1 : Arrays/7_Remove Duplicate From Sorted Array.cpp
@@ -1,7 +1,10 @@
+// Two Pointers
+
+
+// variation 1--> elements present max once
 // Link --> https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 
-// Two Pointers
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -17,4 +20,39 @@ public:
         }
         return i+1;
     }
+};
+
+
+
+
+
+// variation 1--> elements present max twice
+// Link --> https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0; // Edge case: empty array
+        int i = 0; // Pointer for the result array
+        int count = 1; // Count of occurrences of the current element
+        int j = 1; // Traversal pointer
+        // Traverse the array using a while loop
+        while (j < nums.size()) {
+            // If the current element is the same as the previous one, increment the count
+            if (nums[j] == nums[i]) {
+                count++;
+            } else {
+                // If the current element is different, reset the count
+                count = 1;
+            }
+            // If the count is <= 2, copy the element to the result array
+            if (count <= 2) {
+                i++;
+                nums[i] = nums[j];
+            }
+            j++; // Move to the next element
+        }
+        return i + 1; // Return the number of valid elements
+        }
+    
 };

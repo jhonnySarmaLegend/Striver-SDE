544e73fbaed5d229c37508258e65adc41ce33e18
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-06 10:26:52 +0530

    Create 2_Minimum Number of Coins.cpp

---
+++ Day 25 : DP/2_Minimum Number of Coins.cpp
@@ -0,0 +1,9 @@
+// Link --> https://leetcode.com/problems/coin-change/description/
+
+// Coin change(Leetcode)   or  Minimum number of coins to make a Amount(GFG)   or minimum elements(codeStudio)
+
+
+// Greedy Approach Fails:
+//For example, if coins = [1, 3, 4] and amount = 6, the greedy approach (always taking the largest coin) would give 4 + 1 + 1 (3 coins),
+//but the optimal solution is 3 + 3 (2 coins).
+

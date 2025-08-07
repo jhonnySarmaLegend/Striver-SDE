370aeb8971cd4ba5f8c6e7dc6f0945a14cca2c81
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-07 09:23:25 +0530

    Update 3_0-1 Knapsack.cpp

--- Day 25 : DP/3_0-1 Knapsack.cpp
+++ Day 25 : DP/3_0-1 Knapsack.cpp
@@ -13,14 +13,11 @@ using namespace std;
 int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n) {
 
     // Base Case
-    if (n == 0 || W == 0)
-        return 0;
+    if (n == 0 || W == 0)return 0;
 
     int pick = 0;
-
     // Pick nth item if it does not exceed the capacity of knapsack
-    if (wt[n - 1] <= W)
-        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1);
+    if (wt[n - 1] <= W)pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1);
     
     // Don't pick the nth item
     int notPick = knapsackRec(W, val, wt, n - 1);
@@ -58,17 +55,14 @@ int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n,
                                         vector<vector<int>> &dp) {
 
     // Base Case
-    if (n == 0 || W == 0)
-        return 0;
+    if (n == 0 || W == 0)return 0;
 
     // Check if we have previously calculated the same subproblem
     if(dp[n][W] != -1)return dp[n][W];
 
     int pick = 0;
-
     // Pick nth item if it does not exceed the capacity of knapsack
-    if (wt[n - 1] <= W)
-        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1, dp);
+    if (wt[n - 1] <= W)pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1, dp);
     
     // Don't pick the nth item
     int notPick = knapsackRec(W, val, wt, n - 1, dp);

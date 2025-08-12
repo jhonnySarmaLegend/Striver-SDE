d5e1585efafc2748a95b23079635d484b93c3492
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-12 12:29:05 +0530

    Update 6_AssignCookies.cpp

--- Day 8 : Greedy Algorithm/6_AssignCookies.cpp
+++ Day 8 : Greedy Algorithm/6_AssignCookies.cpp
@@ -3,20 +3,21 @@
   Question Link --> https://leetcode.com/problems/assign-cookies/description/
 */
 // T.C. = O(max(M,N)) + O(LogN) ~ O(LogN), S.C. =O(1)
-int findContentChildren(vector<int>& g, vector<int>& s) {
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int ans=0;
         int n=g.size(), m=s.size();
         int i=0,j=0;
-        while(i<m && j<n){
-            if(s[i]>=g[j]){
-                ans++;i++;j++;
+        while(j<m && i<n){
+            if(g[i]<=s[j]){
+                ans++;i++;
             }
-            else{
-                i++;
-            }
+           j++;
             
         }
         return ans;
     }
+};

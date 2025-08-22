b7dda4e16dffbe2869499e84de55d26fdd7fcb25
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-22 09:10:21 +0530

    Update 1_RotateLLRight.cpp

--- Day 7 : Linked List & Arrays/1_RotateLLRight.cpp
+++ Day 7 : Linked List & Arrays/1_RotateLLRight.cpp
@@ -2,7 +2,19 @@
   Question Link --> https://leetcode.com/problems/rotate-list/description/
 */
 
-   ListNode* rotateRight(ListNode* head, int k) {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
 
         if(head==NULL||k==0)return head; // NULL LL or k==0
 
@@ -19,16 +31,17 @@
         while(temp->next!=NULL){
              temp=temp->next;
         }
-        temp->next=head;//last node to 1st
+        temp->next=head;
+
         k=k-1;//since head at node 1
-     
-        for(int i=1;i<count-k;i++)temp=temp->next;
+       
+        for(int i=1;i<count-k;i++)temp=temp->next;   // count-k to readch k nodes from end to rotate
         head=temp->next;
         temp->next=NULL;
         return head;
 
     }
-
+};
 /*
   Time Complexity: O(length of list) + O(length of list - (length of list%k))
 

4be498ba4863dc2af78d2124400ad1a62a0448b8
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-08-26 09:38:40 +0530

    Update 2_KthLargestElementInAStream.cpp

--- Day 12 : Heaps/2_KthLargestElementInAStream.cpp
+++ Day 12 : Heaps/2_KthLargestElementInAStream.cpp
@@ -2,21 +2,20 @@
 
 class KthLargest {
 public:
-
-        int k;
-    priority_queue<int, vector<int>, greater<int>> pq;
-    KthLargest(int k, vector<int>& nums): k(k){
-        for(int x: nums) {
-            pq.push(x);
-            if (pq.size()> k) pq.pop();
+    priority_queue<int,vector<int>,greater<int>>pq;// min heap --> at kth place will be kth largest element
+    int K=0;// global variable to pass k
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+        for(auto it:nums){
+            pq.push(it);
+            if(pq.size()>k)pq.pop();
         }
     }
     
     int add(int val) {
         pq.push(val);
-        if (pq.size()> k) pq.pop();   
-        return pq.top();      
-    
+        if(pq.size()>K)pq.pop();// global variable to pass k here
+        return pq.top();
     }
 };
 

6108f55ec1984d05a053dba3d12b8184446213d1
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-09-08 12:56:53 +0530

    Update 6_LongestSubstringWithoutRepeating.cpp

--- Day 4 : Arrays/6_LongestSubstringWithoutRepeating.cpp
+++ Day 4 : Arrays/6_LongestSubstringWithoutRepeating.cpp
@@ -3,6 +3,35 @@
 */
 
 
+//OPTIMAL METHOD Without comments
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+       vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)left = max(mpp[s[right]] + 1, left);//encountered early
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+      }
+      return len;
+     
+    }
+};
+
+
+
+
+
+
+
+
 // METHOD 1 - using hashmap
 class Solution {
 public:
@@ -22,6 +51,10 @@ public:
     }
 };
 
+
+
+
+// COMMENTED
 // METHOD 2 - using set
 class Solution {
 public:

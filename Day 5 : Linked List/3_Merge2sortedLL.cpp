9c6269c7c5f1f0c847135c6b34b60e90b9a81508
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-09-09 12:46:12 +0530

    Update 3_Merge2sortedLL.cpp

--- Day 5 : Linked List/3_Merge2sortedLL.cpp
+++ Day 5 : Linked List/3_Merge2sortedLL.cpp
@@ -2,6 +2,64 @@
   Question Link --> https://leetcode.com/problems/merge-two-sorted-lists/description/
 */
 
+
+
+//OPTIMAL
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //Edge cases
+		if(list1==NULL && list2==NULL)return NULL;
+        if(list1==NULL && list2!=NULL)return list2;
+        if(list1!=NULL && list2==NULL)return list1;
+
+        ListNode* temp=new ListNode(-1);
+        ListNode *dummy=temp;
+
+        ListNode *temp1=list1;
+        ListNode *temp2=list2;
+
+        while(temp1 && temp2){
+            if(temp1->val<=temp2->val){
+                temp->next=temp1;
+                temp1=temp1->next;
+            }else{
+                temp->next=temp2;
+                temp2=temp2->next;
+            }
+             temp=temp->next;
+        }
+
+        if(temp1!=NULL){
+            temp->next=temp1;
+            temp1=temp1->next;
+        }
+        
+        if(temp2!=NULL){
+            temp->next=temp2;
+            temp2=temp2->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+
+
+
+
+
+
 // brute force
 // put both in set and make LL from set
 // T.C. =O(2(N+M)) , S.C.=O(N+M)

66cc82d37293cda1a2acebc12a07c5b8b8a9ca31
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-09-09 23:55:11 +0530

    Create 7_Odd_Even_LL_by_Value.cpp

---
+++ Day 5 : Linked List/7_Odd_Even_LL_by_Value.cpp
@@ -0,0 +1,52 @@
+// User function template for C++
+
+
+/// Question Link --> https://www.geeksforgeeks.org/problems/segregate-even-and-odd-nodes-in-a-linked-list5035/1
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+// Input: Linked list: 17->15->8->9->2->4->6
+//Output: 8->2->4->6->17->15->9
+// even --> odd
+
+class Solution {
+  public:
+    Node* divide(Node* head) {
+        Node *even=new Node(-1);
+        Node *temp1=even;
+        Node *odd=new Node(-1);
+        Node *temp2=odd;
+        
+        Node *temp=head;
+        while(temp){
+            if(temp->data%2==0){
+                temp1->next=temp;
+                temp=temp->next;
+                temp1=temp1->next;
+            }
+            else{
+                temp2->next=temp;
+                temp=temp->next;
+                temp2=temp2->next;
+            }
+        }
+        
+        temp1->next=odd->next;
+        temp2->next=NULL;
+        return even->next;
+        
+        
+    }
+};

aac1dbd3c15e757d1aafe02371f4c3068cb8ee5b
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-09-29 15:47:46 +0530

    Update 3_Merge2sortedLL.cpp

--- Day 5 : Linked List/3_Merge2sortedLL.cpp
+++ Day 5 : Linked List/3_Merge2sortedLL.cpp
@@ -16,43 +16,32 @@
  * };
  */
 
-class Solution {
-public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)return list2;
+        if(list2==NULL)return list1;
         if(list1==NULL && list2==NULL)return NULL;
-        if(list1==NULL && list2!=NULL)return list2;
-        if(list1!=NULL && list2==NULL)return list1;
 
-        ListNode* dummy=new ListNode(-1);
+        ListNode *dummy=new ListNode(-1);
         ListNode *temp=dummy;
 
-        ListNode *temp1=list1;
-        ListNode *temp2=list2;
-
-        while(temp1 && temp2){
-            if(temp1->val<=temp2->val){
-                temp->next=temp1;
-                temp1=temp1->next;
-            }else{
-                temp->next=temp2;
-                temp2=temp2->next;
+        while(list1 && list2){
+            if(list1->val<=list2->val){
+                temp->next=list1;
+                list1=list1->next;
             }
-             temp=temp->next;
+            else{
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
         }
-
-          if(temp1!=NULL){
-            temp->next=temp1;
-            temp1=temp1->next;
-           }
-           if(temp2!=NULL){
-            temp->next=temp2;
-            temp2=temp2->next;
-           }
-
-           return dummy->next;
+ 
+        if (list1) temp->next = list1;
+        if (list2) temp->next = list2;
+        
+        return dummy->next;
+ 
     }
-};
-
 
 
 

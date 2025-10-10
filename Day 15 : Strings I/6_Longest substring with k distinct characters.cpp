b0db50b29eb650bbd0a34e4671159eb8b1b35fee
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-10-10 17:10:45 +0530

    Update 6_Longest substring with k distinct characters.cpp

--- Day 15 : Strings I/6_Longest substring with k distinct characters.cpp
+++ Day 15 : Strings I/6_Longest substring with k distinct characters.cpp
@@ -26,3 +26,23 @@ while (right < s.size()) {
 
 return maxLen;
 }
+
+
+
+
+/* Longest substring with  distinct characters
+ vector < int > mpp(256, -1);
+
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)left = max(mpp[s[right]] + 1, left);//encountered early --> for vector index 
+
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
+      }
+      return len
+          */

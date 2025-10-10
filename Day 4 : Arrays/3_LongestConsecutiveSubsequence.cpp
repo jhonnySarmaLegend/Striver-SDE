579f48a3a8fe90bf0606faaa318c7771d7891f8d
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-10-10 08:58:34 +0530

    Update 3_LongestConsecutiveSubsequence.cpp

--- Day 4 : Arrays/3_LongestConsecutiveSubsequence.cpp
+++ Day 4 : Arrays/3_LongestConsecutiveSubsequence.cpp
@@ -1,32 +1,25 @@
 /*
   Question Link --> https://leetcode.com/problems/longest-consecutive-sequence/description/
 */
-
-int longestSuccessiveElements(vector<int>&a) {
-    int n = a.size();
-    if (n == 0) return 0;
-
-    int longest = 1;
-    unordered_set<int> st; // S.C.=O(N)
-    //put all the array elements into set:
-    for (int i = 0; i < n; i++) {
-        st.insert(a[i]);
-    }
-
-    //Find the longest sequence:
-    for (auto it : st) { // T.C.=O(N)
-        //if 'it' is a starting number:
-        if (st.find(it - 1) == st.end()) {
-            //find consecutive numbers:
-            int cnt = 1;
-            int x = it;
-            while (st.find(x + 1) != st.end()) {
-                x = x + 1;
-                cnt = cnt + 1;
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int maxi=0;//  if (nums.empty()) return 0; else would have assigned INT_MIN
+        int cnt=0;
+        set<int> s(nums.begin(),nums.end());// copy nums to set
+        
+        for(auto it:s){
+            //nums[i] is the starting index
+            if(s.find(it-1)==s.end()){
+                cnt=1;
+                int start=it;
+                while(s.find(start+1)!=s.end()){
+                  cnt++;
+                  start++;
+                }
+                maxi=max(maxi,cnt);
             }
-            longest = max(longest, cnt);
         }
+        return maxi;
     }
-    return longest;
-
-}
+};

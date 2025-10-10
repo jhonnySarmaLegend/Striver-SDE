e99708dd0c22a77752166800e2924c657eb25b7a
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-10-10 09:47:39 +0530

    Update 7_Max_Product_Subarray.cpp

--- Day 4 : Arrays/7_Max_Product_Subarray.cpp
+++ Day 4 : Arrays/7_Max_Product_Subarray.cpp
@@ -17,3 +17,18 @@
         return maxi;
     }
 
+
+//Shorter code
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suff=1;
+        int maxi=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            if(pre==0)pre=1;pre*=nums[i];
+            if(suff==0)suff=1;suff*=nums[nums.size()-i-1];
+            maxi=max(maxi,max(pre,suff));
+        }
+        return maxi;
+    }
+};

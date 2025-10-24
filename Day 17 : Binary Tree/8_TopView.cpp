280e7a6a36db46c6f6a50c5c3099fe62ce060106
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-10-24 17:07:30 +0530

    Update 8_TopView.cpp

--- Day 17 : Binary Tree/8_TopView.cpp
+++ Day 17 : Binary Tree/8_TopView.cpp
@@ -3,6 +3,46 @@
 // First Node of every LINE -- Level order traversal
 
     // top view of the binary tree
+
+
+vector<int> topView(Node* root){
+        vector<int> ans;
+        if(root == NULL)return ans;
+        
+        map<int, int> mpp;
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+    
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            
+            if(mpp.find(line) == mpp.end())mpp[line] = node->data;//First Occurence
+            
+            if(node->left != NULL)q.push({node->left, line - 1});
+            
+            if(node->right != NULL)q.push({node->right, line + 1}); 
+        }
+    
+        for(auto it : mpp)ans.push_back(it.second);
+    
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+//COMMENTED
     vector<int> topView(Node* root){
         // Vector to store the result
         vector<int> ans;
